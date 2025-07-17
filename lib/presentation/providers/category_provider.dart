dc8c8e0bd0b71c6a24720f23f765edfe157a4823
Mateusz Bober <mateusz.bober@gmail.com>
2025-07-17 09:17:11 +0200

    removed redundant file

--- lib/presentation/providers/category_provider.dart
+++
@@ -1,6 +0,0 @@
-import 'package:dam/data/models/category_model.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final categoryProvider = ChangeNotifierProvider<Category>((ref) {
-  return Category(name: '');
-});
